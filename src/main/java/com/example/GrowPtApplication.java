8ae59efb60b8564b9ddc82b7309ccf8b255657da
AyoungJo <joahae0302@gmail.com>
2023-07-07 15:33:12 +0900

    first commit

---
+++ src/main/java/com/example/GrowPtApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowPtApplication.class, args);
+	}
+
+}
